1e19d6caca75ebded379c5ec4ac24fe6fcf2f1a8
guidfort <guidfort@bosch.com>
2024-09-04 09:40:01 +0100

    Deployed 2a6f4d2 to 2.4.3 with MkDocs 1.6.1 and mike 2.1.3

---
+++ 2.4.3/common.log.diagnosis/diagnosis_templates/TEMPLATE_main_diag.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "common/log/diagnosis/defs.h"
+
+
+namespace <NAMESPACE>
+{
+  extern const common::log::diagnosis::DiagnosisElement MAIN_DIAGNOSIS_LIST[];
+  extern const std::uint32_t MAIN_DIAGNOSIS_LIST_SIZE;
+
+  constexpr std::uint32_t MAIN_DIAG_<MAIN_DIAG_1_IDENTIFIER> = 0x<MAIN_DIAG_1_CODE>;
+  constexpr std::uint32_t MAIN_DIAG_<MAIN_DIAG_2_IDENTIFIER> = 0x<MAIN_DIAG_2_CODE>;
+}
